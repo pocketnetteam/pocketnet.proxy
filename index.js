9f3b99542a45d3e118997aea6b8094d79c402052
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2024-10-08 15:47:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = {
     kit: require("./src/kit.js"),
     cli: require("./src/cli.js"),
     functions: require("./src/functions.js"),
+    pocketcoin: require('./src/lib/btc16.js')
 };
