75a91f0726fcd0dc5adb5e847e9c40c9ad0d5b3e
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2024-10-09 12:55:49 +0300

    Update test.js

--- test.js
+++ test.js
@@ -8,10 +8,7 @@ global.USE_TLS_NODES_ONLY = true
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 */
 
-
 const kit = require("./src/kit.js")
-const f = require("./src/functions.js");
-
 
 kit.start({list : ['cache', 'nodeManager']}).then(() => {
 
@@ -28,23 +25,3 @@ kit.start({list : ['cache', 'nodeManager']}).then(() => {
 }).catch(e => {
     console.log(e)
 })
-/*
-kit.init({
-
-    server: {
-        enabled: true
-    }
-
-}).catch(r => {
-
-    console.log(r)
-    return Promise.resolve()
-
-}).then(r => {
-
-    ['SIGINT', 'SIGTERM', 'SIGQUIT']
-        .forEach(signal => process.on(signal, () => {
-            kit.destroy()
-            process.exit(0)
-        }))
-})*/
